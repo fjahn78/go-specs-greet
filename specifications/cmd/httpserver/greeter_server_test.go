fb8dfb9680aafcdec046045fb8b4d91790e14ea6
Frank Jahn <frank.jahn@lfst.fin-rlp.de>
2025-05-30 07:06:10 +0200

    Pre RED

---
+++ specifications/cmd/httpserver/greeter_server_test.go
@@ -0,0 +1,12 @@
+package main_test
+
+import (
+	"testing"
+
+	"lsscmp.fin-rlp.local/ZD0436admin/go-specs-greet/specifications"
+)
+
+func TestGreeterServer(t *testing.T) {
+	driver := go_specs_greet.Driver{BaseURL: "http://localhost:8080"}
+	specifications.GreetSpecification(t, driver)
+}
--- specifications/greet.go
+++ specifications/greet.go
@@ -9,7 +9,7 @@ type Greeter interface {
 	Greet() (string, error)
 }
 
-func GreetSecification(t testing.TB, greeter Greeter)  {
+func GreetSpecification(t testing.TB, greeter Greeter)  {
 	got, err := greeter.Greet()
 	assert.NoError(t, err)
 	assert.Equal(t, got, "Hello, world")
