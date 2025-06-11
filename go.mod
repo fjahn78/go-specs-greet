d80e20adad88c9bcfd88977954c97450974258d3
Frank Jahn <frank.jahn@lfst.fin-rlp.de>
2025-06-11 10:16:05 +0200

    add testcontainers

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module lsscmp.fin-rlp.local/ZD0436admin/go-specs-greet
 
 go 1.24.2
 
-require github.com/alecthomas/assert/v2 v2.11.0
+require (
+	github.com/alecthomas/assert/v2 v2.11.0
+	github.com/testcontainers/testcontainers-go v0.37.0
+)
 
 require (
 	github.com/alecthomas/repr v0.4.0 // indirect
--- specifications/cmd/httpserver/greeter_server_test.go
+++ specifications/cmd/httpserver/greeter_server_test.go
@@ -1,13 +1,37 @@
 package main_test
 
 import (
+	"context"
 	"testing"
 
+	"github.com/alecthomas/assert/v2"
+	tc "github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 	go_specs_greet "lsscmp.fin-rlp.local/ZD0436admin/go-specs-greet"
 	"lsscmp.fin-rlp.local/ZD0436admin/go-specs-greet/specifications"
 )
 
 func TestGreeterServer(t *testing.T) {
+	ctx := context.Background()
+
+	req := tc.ContainerRequest{
+		FromDockerfile: tc.FromDockerfile{
+			Context: "../../.",
+			Dockerfile: "./cmd/httpserver/Dockerfile",
+			PrintBuildLog: true,
+		},
+		ExposedPorts: []string{"8080:8080"},
+		WaitingFor:   wait.ForHTTP("/").WithPort("8080"),
+	}
+	container, err := tc.GenericContainer(ctx, tc.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:		  true,
+	})
+	assert.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, container.Terminate(ctx))
+	})
+	
 	driver := go_specs_greet.Driver{BaseURL: "http://localhost:8080"}
 	specifications.GreetSpecification(t, driver)
 }
