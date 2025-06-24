52c97007619c24a380624dba1af4e79652087404
Frank Jahn <10794823+fjahn78@users.noreply.github.com>
2025-06-24 14:50:18 +0200

    should pass

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net"
 
-func main(){
-	fmt.Println("implement me!")
+	"github.com/fjahn78/go-specs-greet/adapters/grpcserver"
+	"google.golang.org/grpc"
+)
+
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/quii/go-specs-greet/adapters/grpcserver"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := grpc.NewServer()
+	grpcserver.RegisterGreeterServer(s, &GreetServer{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type GreetServer struct {
+	grpcserver.UnimplementedGreeterServer
+}
+
+func (g GreetServer) Greet(ctx context.Context, request *grpcserver.GreetRequest) (*grpcserver.GreetReply, error) {
+	return &grpcserver.GreetReply{Message: "fixme"}, nil
 }
