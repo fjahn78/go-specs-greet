161b87a2f6e18300ea937e0f96c5ad6c580b7e8c
Frank Jahn <fjahn78@gmail.com>
2025-07-04 14:50:55 +0200

    GREEN

--- adapters/httpserver/driver.go
+++ adapters/httpserver/driver.go
@@ -10,6 +10,25 @@ type Driver struct {
 	Client  *http.Client
 }
 
+// Curse implements specifications.MeanGreeter.
+func (d Driver) Curse(name string) (string, error) {
+	return newFunction(d, name)
+}
+
+func newFunction(d Driver, name string) (string, error) {
+	res, err := d.Client.Get(d.BaseURL + "/curse?name=" + name)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close() //nolint:all
+
+	greeting, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(greeting), nil
+}
+
 func (d Driver) Greet(name string) (string, error) {
 	res, err := d.Client.Get(d.BaseURL + "/greet?name=" + name)
 	if err != nil {
--- adapters/httpserver/handler.go
+++ adapters/httpserver/handler.go
@@ -2,15 +2,26 @@ package httpserver
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
-	gsg "github.com/fjahn78/go-specs-greet/domain/interactions"
+	"github.com/fjahn78/go-specs-greet/domain/interactions"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	if _, err := fmt.Fprint(w, gsg.Greet(name)); err!= nil {
-		log.Fatal(err)
+const (
+	greetPath = "/greet"
+	cursePath = "/curse"
+)
+
+func NewHandler() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc(greetPath, replyWith(interactions.Greet))
+	mux.HandleFunc(cursePath, replyWith(interactions.Curse))
+	return mux
+}
+
+func replyWith(f func(name string) (interaction string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Query().Get("name")
+		fmt.Fprint(w, f(name))
 	}
 }
